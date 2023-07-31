c626501a75c535ba266f258d68448b0056408499
Timothy <timothyhumphries06@gmail.com>
2023-07-31 09:59:30 -0600

    Commit

---
+++ src/simulation/Particle.cpp
@@ -0,0 +1,37 @@
+#include <cstddef>
+#include "Particle.h"
+
+std::vector<StructProperty> const &Particle::GetProperties()
+{
+	static std::vector<StructProperty> properties = {
+		{ "type"   , StructProperty::ParticleType, (intptr_t)(offsetof(Particle, type   )) },
+		{ "life"   , StructProperty::Integer     , (intptr_t)(offsetof(Particle, life   )) },
+		{ "ctype"  , StructProperty::ParticleType, (intptr_t)(offsetof(Particle, ctype  )) },
+		{ "x"      , StructProperty::Float       , (intptr_t)(offsetof(Particle, x      )) },
+		{ "y"      , StructProperty::Float       , (intptr_t)(offsetof(Particle, y      )) },
+		{ "vx"     , StructProperty::Float       , (intptr_t)(offsetof(Particle, vx     )) },
+		{ "vy"     , StructProperty::Float       , (intptr_t)(offsetof(Particle, vy     )) },
+		{ "temp"   , StructProperty::Float       , (intptr_t)(offsetof(Particle, temp   )) },
+		{ "flags"  , StructProperty::UInteger    , (intptr_t)(offsetof(Particle, flags  )) },
+		{ "tmp"    , StructProperty::Integer     , (intptr_t)(offsetof(Particle, tmp    )) },
+		{ "tmp2"   , StructProperty::Integer     , (intptr_t)(offsetof(Particle, tmp2   )) },
+		{ "tmp3"   , StructProperty::Integer     , (intptr_t)(offsetof(Particle, tmp3   )) },
+		{ "tmp4"   , StructProperty::Integer     , (intptr_t)(offsetof(Particle, tmp4   )) },
+		{ "dcolour", StructProperty::UInteger    , (intptr_t)(offsetof(Particle, dcolour)) },
+		{ "o2"    , StructProperty::Integer      , (intptr_t)(offsetof(Particle, bio.o2    )) },
+		{ "co2"   , StructProperty::Integer      , (intptr_t)(offsetof(Particle, bio.co2   )) },
+		{ "health"   , StructProperty::Integer   , (intptr_t)(offsetof(Particle, bio.health   )) },
+		{ "glucose"   , StructProperty::Integer   , (intptr_t)(offsetof(Particle, bio.glucose   )) },
+	};
+	return properties;
+}
+
+std::vector<StructPropertyAlias> const &Particle::GetPropertyAliases()
+{
+	static std::vector<StructPropertyAlias> aliases = {
+		{ "pavg0" , "tmp3"    },
+		{ "pavg1" , "tmp4"    },
+		{ "dcolor", "dcolour" },
+	};
+	return aliases;
+}
---
+++ src/simulation/elements/HOT.cpp
@@ -0,0 +1,54 @@
+#include "simulation/ElementCommon.h"
+
+static int update(UPDATE_FUNC_ARGS);
+
+void Element::Element_HOT()
+{
+	Identifier = "DEFAULT_PT_HOT";
+	Name = "HOT";
+	Colour = PIXPACK(0x990000);
+	MenuVisible = 1;
+	MenuSection = SC_NUCLEAR;
+	Enabled = 1;
+
+	Advection = 0.4f;
+	AirDrag = 0.01f * CFDS;
+	AirLoss = 0.99f;
+	Loss = 0.95f;
+	Collision = 0.0f;
+	Gravity = 0.4f;
+	Diffusion = 0.00f;
+	HotAir = 0.000f	* CFDS;
+	Falldown = 1;
+
+	Flammable = 0;
+	Explosive = 0;
+	Meltable = 0;
+	Hardness = 0;
+	PhotonReflectWavelengths = 0x003FC000;
+
+	Weight = 100;
+
+	DefaultProperties.temp = R_TEMP + 30.0f + 273.15f;
+	HeatConduct = 255;
+	Description = "Heats up ridiculously quickly.";
+
+	Properties = TYPE_PART | PROP_RADIOACTIVE;
+
+	LowPressure = IPL;
+	LowPressureTransition = NT;
+	HighPressure = IPH;
+	HighPressureTransition = NT;
+	LowTemperature = ITL;
+	LowTemperatureTransition = NT;
+	HighTemperature = ITH;
+	HighTemperatureTransition = NT;
+
+	Update = &update;
+}
+
+static int update(UPDATE_FUNC_ARGS)
+{
+			parts[i].temp += 1000.0f;
+	return 0;
+}
