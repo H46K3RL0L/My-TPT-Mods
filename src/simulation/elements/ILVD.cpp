28bb31b945df21e2c4bf07198e39bb3e1175fc60
Nobody <88124759+H46K3RL0L@users.noreply.github.com>
2023-08-02 10:57:37 -0600

    Github really hates me. every time i want to push i have to delete .git and push everything again.

---
+++ src/simulation/elements/ILVD.cpp
@@ -0,0 +1,42 @@
+#include "simulation/ElementCommon.h"
+
+void Element::Element_ILVD()
+{
+	Identifier = "DEFAULT_PT_ILVD";
+	Name = "ILVD";
+	Colour = PIXPACK(0x701e1b);
+	MenuVisible = 1;
+	MenuSection = SC_SPECIAL;
+	Enabled = 1;
+
+	Advection = 0.0f;
+	AirDrag = 0.00f * CFDS;
+	AirLoss = 0.95f;
+	Loss = 0.00f;
+	Collision = 0.0f;
+	Gravity = 0.0f;
+	Diffusion = 0.00f;
+	HotAir = 0.000f	* CFDS;
+	Falldown = 0;
+
+	Flammable = 0;
+	Explosive = 0;
+	Meltable = 0;
+	Hardness = 10;
+
+	Weight = 100;
+
+	HeatConduct = 0;
+	Description = "Insulated void, acts like void but does not conduct heat or electricity.";
+
+	Properties = TYPE_SOLID;
+
+	LowPressure = IPL;
+	LowPressureTransition = NT;
+	HighPressure = IPH;
+	HighPressureTransition = NT;
+	LowTemperature = ITL;
+	LowTemperatureTransition = NT;
+	HighTemperature = ITH;
+	HighTemperatureTransition = NT;
+}
